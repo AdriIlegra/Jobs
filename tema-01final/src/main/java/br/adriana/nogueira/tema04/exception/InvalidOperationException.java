2a3c938c3b3cdc772f0ff4ffb7344e8c66544668
Adriana Borges Nogueira <129796868+AdriIlegra@users.noreply.github.com>
2024-01-30 19:57:06 -0300

    Delete tema-01final directory

--- tema-01final/src/main/java/br/adriana/nogueira/tema04/exception/InvalidOperationException.java
+++
@@ -1,8 +0,0 @@
-package br.adriana.nogueira.tema04.exception;
-
-public class InvalidOperationException extends RuntimeException {
-
-    public InvalidOperationException(String message) {
-        super(message);
-    }
-}
